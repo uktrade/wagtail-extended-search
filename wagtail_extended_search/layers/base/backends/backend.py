99793b9eb87d8dcf5108854585349651614df04d
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-02-13 12:37:26 +0000

    Move `_remap_fields` where it belongs

--- wagtail_extended_search/layers/base/backends/backend.py
+++ wagtail_extended_search/layers/base/backends/backend.py
@@ -1,12 +1,10 @@
-from typing import Optional, Union
+from typing import Union
 
 from wagtail.search.backends.elasticsearch7 import (
     Elasticsearch7SearchQueryCompiler,
     Field,
 )
 from wagtail.search.query import Fuzzy, MatchAll, Not, Phrase, PlainText
-
-from wagtail_extended_search import settings as search_settings
 
 
 class ExtendedSearchQueryCompiler(Elasticsearch7SearchQueryCompiler):
@@ -31,65 +29,6 @@ class ExtendedSearchQueryCompiler(Elasticsearch7SearchQueryCompiler):
         boostable_fields = [self.to_field(f) for f in fields]
 
         return super().get_boosted_fields(boostable_fields)
-
-    def _remap_fields(
-        self,
-        fields,
-        get_searchable_fields__args: Optional[tuple] = None,
-        get_searchable_fields__kwargs: Optional[dict] = None,
-    ):
-        """
-        Convert field names into index column names
-        """
-        if get_searchable_fields__args is None:
-            get_searchable_fields__args = ()
-        if get_searchable_fields__kwargs is None:
-            get_searchable_fields__kwargs = {}
-
-        if not fields:
-            return super()._remap_fields(fields)
-
-        remapped_fields = []
-
-        searchable_fields = {
-            f.field_name: f
-            for f in self.get_searchable_fields(
-                *get_searchable_fields__args,
-                **get_searchable_fields__kwargs,
-            )
-        }
-
-        for field_name in fields:
-            field = searchable_fields.get(field_name)
-            if field:
-                field_name = self.mapping.get_field_column_name(field)
-                remapped_fields.append(Field(field_name, field.boost or 1))
-            else:
-                # @TODO this works but ideally we'd move get_field_column_name to handle this directly
-                field_name_parts = field_name.split(".")
-                if field_name_parts[0] in searchable_fields:
-                    parent_related_field = searchable_fields[field_name_parts[0]]
-                    field_name = self.mapping.get_field_column_name(
-                        parent_related_field
-                    )
-                    field_name_remainder = ".".join(field_name_parts[1:])
-                    field_name = f"{field_name}.{field_name_remainder}"
-
-                    # Get the field boost from the settings so it can be managed in the DB.
-                    child_field = parent_related_field.get_related_field(
-                        field_name_remainder
-                    )
-                    field_settings_key = search_settings.get_settings_field_key(
-                        self.queryset.model, child_field
-                    )
-                    field_boost = float(
-                        search_settings.wagtail_extended_search_settings["boost_parts"][
-                            "fields"
-                        ].get(field_settings_key, 1)
-                    )
-                    remapped_fields.append(Field(field_name, boost=field_boost))
-
-        return remapped_fields
 
     def _join_and_compile_queries(self, query, fields, boost=1.0):
         """
--- wagtail_extended_search/layers/only_fields/backends/backend.py
+++ wagtail_extended_search/layers/only_fields/backends/backend.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
+from wagtail.search.backends.elasticsearch7 import Field
 from wagtail.search.index import SearchField
 from wagtail.search.query import MATCH_NONE
 
+from wagtail_extended_search import settings as search_settings
 from wagtail_extended_search.layers.base.backends.backend import (
     ExtendedSearchQueryCompiler,
 )
@@ -14,6 +18,65 @@ class OnlyFieldSearchQueryCompiler(ExtendedSearchQueryCompiler):
     the ExtendedSearchQueryCompiler; this exists to support the new OnlyFields
     SearchQuery
     """
+
+    def _remap_fields(
+        self,
+        fields,
+        get_searchable_fields__args: Optional[tuple] = None,
+        get_searchable_fields__kwargs: Optional[dict] = None,
+    ):
+        """
+        Convert field names into index column names
+        """
+        if get_searchable_fields__args is None:
+            get_searchable_fields__args = ()
+        if get_searchable_fields__kwargs is None:
+            get_searchable_fields__kwargs = {}
+
+        if not fields:
+            return super()._remap_fields(fields)
+
+        remapped_fields = []
+
+        searchable_fields = {
+            f.field_name: f
+            for f in self.get_searchable_fields(
+                *get_searchable_fields__args,
+                **get_searchable_fields__kwargs,
+            )
+        }
+
+        for field_name in fields:
+            field = searchable_fields.get(field_name)
+            if field:
+                field_name = self.mapping.get_field_column_name(field)
+                remapped_fields.append(Field(field_name, field.boost or 1))
+            else:
+                # @TODO this works but ideally we'd move get_field_column_name to handle this directly
+                field_name_parts = field_name.split(".")
+                if field_name_parts[0] in searchable_fields:
+                    parent_related_field = searchable_fields[field_name_parts[0]]
+                    field_name = self.mapping.get_field_column_name(
+                        parent_related_field
+                    )
+                    field_name_remainder = ".".join(field_name_parts[1:])
+                    field_name = f"{field_name}.{field_name_remainder}"
+
+                    # Get the field boost from the settings so it can be managed in the DB.
+                    child_field = parent_related_field.get_related_field(
+                        field_name_remainder
+                    )
+                    field_settings_key = search_settings.get_settings_field_key(
+                        self.queryset.model, child_field
+                    )
+                    field_boost = float(
+                        search_settings.wagtail_extended_search_settings["boost_parts"][
+                            "fields"
+                        ].get(field_settings_key, 1)
+                    )
+                    remapped_fields.append(Field(field_name, boost=field_boost))
+
+        return remapped_fields
 
     def get_searchable_fields(self, *args, only_model, **kwargs):
         if not only_model:
@@ -60,4 +123,8 @@ class OnlyFieldSearchQueryCompiler(ExtendedSearchQueryCompiler):
             # Exclude this field from any further downstream compilation: it
             # was defined in the search() method but has been excluded from
             # this part of the tree with an OnlyFields filter
+            return self._compile_query(MATCH_NONE, field, boost)
+            return self._compile_query(MATCH_NONE, field, boost)
+            # this part of the tree with an OnlyFields filter
+            return self._compile_query(MATCH_NONE, field, boost)
             return self._compile_query(MATCH_NONE, field, boost)
